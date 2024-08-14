454812b8ffc7c0e864602bff962570b33bd4ee01
unknown <JhojamCaraballo@gmail.com>
2024-08-14 13:52:34 -0500

    Se Sube Login Del Front

---
+++ Front-End/src/App.jsx
@@ -0,0 +1,40 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <div className="Formulario">
+                <form>
+                    <div className="Titulo">
+                        <h1>Iniciar Sesión</h1>
+                    </div>
+                    <div className="Campo">
+                        <label htmlFor="user">Usuario:</label>
+                        <input type="text" id="user" required/>
+                    </div>
+                    <div className="Campo">
+                        <label htmlFor="password">Contraseña:</label>
+                        <input type="password" id="password" required/>
+                    </div>
+                    <div className="BotonesYCheckbox">
+                        <button type="submit">Iniciar Sesión</button>
+                        <div className="Checkbox">
+                            <label>
+                                <input type="checkbox"/>
+                                Recuérdame
+                            </label>
+                        </div>
+                    </div>
+                    <div className="OlvidasteContra">
+                        <a href="/Recuperacion">¿Olvidaste tu contraseña?</a>
+                    </div>
+                    <div className="registro">
+                    <p>¿No tienes cuenta aún? <a href="/Registro">Registrar</a></p>
+                    </div>
+                    </form>
+            </div>
+        </>
+    )
+}
+
+export default App
