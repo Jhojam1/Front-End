b6f05913889204f8e65baf88b52ec7d56420cdda
Jhojam1 <JhojamCaraballo@gmail.com>
2024-08-27 11:39:09 -0500

    aSe Cambia Al Login

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Wallpaper from './componentes/Wallpaper.jsx';
 import Login from "./Menus/Login.jsx";
-import HomePage from "./componentes/Menu/HomePage.jsx";
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <Wallpaper />
-      <div className="Contenedor">
-          <HomePage />
+      <div className="content">
+          <Login />
       </div>
   </StrictMode>,
 )
