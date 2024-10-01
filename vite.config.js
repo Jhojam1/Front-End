b138c7b7e6b99edecf502883dd5046e912dc54e8
Jhojam1 <115126695+Jhojam1@users.noreply.github.com>
2024-10-01 13:54:18 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Asegúrate de que la carpeta de salida sea 'dist'
-  },
 })
